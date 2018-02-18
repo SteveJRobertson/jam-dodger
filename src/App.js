a10c9df5b8fdc47b1855c543715e964d899c74fb
Steve Robertson <steve@sr.digital>
2018-02-18 20:59:00 +0000

    Card markup complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,28 @@ class App extends Component {
     return (
       <div className="App">
         <Header title={appTitle} />
+
+        <div className="ui container">
+          <div className="column">
+            <div class="ui cards">
+              <div className="card jd-card">
+                <div className="content">
+                  <img
+                    className="right floated mini ui image"
+                    src="https://www.placecage.com/35/35"
+                  />
+                  <div className="header">NicholasCage</div>
+                  <div className="meta">5 minutes ago</div>
+                  <div className="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus eu varius leo, nec feugiat felis. Etiam non
+                    iaculis urna, eget tincidunt sed.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
