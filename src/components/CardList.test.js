3ad659eefc3b600597cdc7cdee42281765691544
Steve Robertson <steve@sr.digital>
2018-03-01 10:53:57 +0000

    Updated unit tests to 100% code coverage

--- src/components/CardList.test.js
+++ src/components/CardList.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import fetch from 'isomorphic-fetch';
 import { shallow, mount } from 'enzyme';
 import CardList from './CardList';
+
 import fetchData from '../api/fetchData';
 
 describe('CardList component', () => {
@@ -14,13 +15,23 @@ describe('CardList component', () => {
 
   describe('fetch data from the api', () => {
     let mockFetchTrafficData;
+    const trafficData = [
+      {
+        created_at: '',
+        id: 1,
+        text: '',
+        user: {
+          profile_image_url: '',
+          screen_name: '',
+        },
+      },
+    ];
 
     beforeEach(() => {
-      const trafficData = {
-        statuses: [],
-      };
       fetch = jest.fn().mockReturnValue(Promise.resolve(trafficData));
-      mockFetchTrafficData = jest.spyOn(fetchData, 'traffic');
+      mockFetchTrafficData = jest.spyOn(fetchData, 'traffic').mockReturnValue(Promise.resolve({
+        statuses: trafficData,
+      }));
       component = mount(<CardList />);
     });
 
@@ -32,6 +43,8 @@ describe('CardList component', () => {
       expect(mockFetchTrafficData).toHaveBeenCalledWith(fetch);
     });
 
-    it('returns the data');
+    it('returns the data', () => {
+      expect(component.state().posts).toEqual(trafficData);
+    });
   });
 });
