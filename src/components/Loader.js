e521a1504ea8a67f71618ccf6de22af39724881b
Steve Robertson <steve@sr.digital>
2018-03-12 20:54:26 +0000

    #36 Updated props with spinner sizes

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 /**
  * The loading spinner used when the app is initially loaded.
  */
-const Loader = props => (
-  <div className="ui active centered inline massive text loader">{props.text}</div>
-);
+class Loader extends Component {
+  loaderClasses() {
+    const sizeClass = this.props.size !== null ? `${this.props.size} ` : '';
+    const textClass = this.props.text !== null ? 'text ' : '';
+    return `ui active centered inline ${sizeClass}${textClass}loader`;
+  }
+
+  render() {
+    console.log(this.loaderClasses());
+    return <div className={this.loaderClasses()}>{this.props.text}</div>;
+  }
+}
 
 Loader.defaultProps = {
-  text: '',
+  size: 'massive',
+  text: null,
 };
 
 Loader.propTypes = {
+  size: PropTypes.string,
   text: PropTypes.string,
 };
 
--- src/components/Loader.md
+++ src/components/Loader.md
@@ -6,8 +6,51 @@
 <Loader />
 ```
 
-#### Example with props
+#### Examples with props
+
+The size of the loader can be adjusted by using the Semantic UI loader size clases. The default
+class is 'massive'. Text is optional.
+
+---
 
 ```js
-<Loader text={'Please wait...'} />
+<Loader
+  size={'mini'}
+  text={'Mini loader'}
+/>
+```
+
+```js
+<Loader
+  size={'tiny'}
+  text={'Tiny loader'}
+/>
+```
+
+```js
+<Loader
+  size={'small'}
+  text={'Small loader'}
+/>
+```
+
+```js
+<Loader
+  size={'medium'}
+  text={'Medium loader'}
+/>
+```
+
+```js
+<Loader
+  size={'large'}
+  text={'Large loader'}
+/>
+```
+
+```js
+<Loader
+  size={'massive'}
+  text={'Massive loader'}
+/>
 ```
